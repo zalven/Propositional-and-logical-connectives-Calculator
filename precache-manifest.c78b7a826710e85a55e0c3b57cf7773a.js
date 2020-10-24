6406f742d6674033af4dfa0387f7f106573df582
zlsdayao <zlsdayao@rpinnotech.com>
2020-10-25 01:04:27 +0800

    Updates

---
+++ precache-manifest.c78b7a826710e85a55e0c3b57cf7773a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16476e85fb2fd1ed507e3a4f3c4db961",
+    "url": "/Propositional-and-logical-connectives-Calculator/index.html"
+  },
+  {
+    "revision": "3d58279c1f0934cd27e8",
+    "url": "/Propositional-and-logical-connectives-Calculator/static/css/main.0fa32d84.chunk.css"
+  },
+  {
+    "revision": "764f92220c60baeecf76",
+    "url": "/Propositional-and-logical-connectives-Calculator/static/js/2.648935bf.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/Propositional-and-logical-connectives-Calculator/static/js/2.648935bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d58279c1f0934cd27e8",
+    "url": "/Propositional-and-logical-connectives-Calculator/static/js/main.75d0befd.chunk.js"
+  },
+  {
+    "revision": "39693c9ff86584b9da32",
+    "url": "/Propositional-and-logical-connectives-Calculator/static/js/runtime-main.cb9d3f64.js"
+  }
+]);
